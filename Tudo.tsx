f918a2f6b926ce4137da4fc9136a678b0a4018df
yangyao <873241789@qq.com>
2020-03-14 14:28:51 +0800

    测试部署自动刷新

--- Tudo.tsx
+++ Tudo.tsx
@@ -4,7 +4,7 @@ export default class Tudo extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, world  yoyo sang!</h1>
       </div>
     );
   }
