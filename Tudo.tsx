86b27778d3a1b9ede0e4db544efac860171f35fd
yangyao <873241789@qq.com>
2020-03-14 14:03:10 +0800

    init

---
+++ Tudo.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class Tudo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world yoyo !</h1>
+      </div>
+    );
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import * as React from "react";
+import Tudo from "./Tudo";
+
+const application = document.getElementById("application");
+
+ReactDOM.render(<Tudo />, application);
